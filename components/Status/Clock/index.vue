cd1abb83da1a28a1a1148aee9e465027c227ccd9
VaroCode <varo4code@gmail.com>
2025-02-17 22:47:36 +0100

    Functional clock

--- components/Status/Clock/index.vue
+++ components/Status/Clock/index.vue
@@ -1,5 +1,33 @@
 <template>
-    <div class="flex items-center gap-2">
-        <span class="text-white font-[Poppins] font-extralight text-3xl tracking-wide uppercase">11:04 pm</span>
-    </div>
+  <div class="flex items-center gap-2">
+    <span
+      class="text-white font-[Poppins] font-extralight text-3xl tracking-wide uppercase"
+    >
+      {{ currentTime }}
+    </span>
+  </div>
 </template>
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.extend(customParseFormat);
+
+const currentTime = ref(dayjs().format("hh:mm A"));
+let timer = null;
+
+onMounted(() => {
+  timer = setInterval(() => {
+    currentTime.value = dayjs().format("hh:mm A");
+  }, 1000);
+});
+
+onUnmounted(() => {
+  clearInterval(timer);
+});
+</script>
